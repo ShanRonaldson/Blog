cdaef66df5c2aa7d94ce1ae55b4f7b6d239cf520
Shannon Schrauwen <shannon.schrauwen@gmail.com>
2020-08-25 16:36:55 +0300

    creating files, making pretty

--- app.js
+++ app.js
@@ -16,8 +16,8 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 app.get("/", function(req, res){
-    res.render("list")
-})
+    res.render("list");
+});
 
 
 app.listen(process.env.PORT || 3000, function(){
