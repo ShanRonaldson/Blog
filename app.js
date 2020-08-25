fabf70958bf2632d92a9b1fe31ab18fc30959272
Shannon Schrauwen <shannon.schrauwen@gmail.com>
2020-08-25 15:06:47 +0300

    creating server and files

--- app.js
+++ app.js
@@ -1,1 +1,26 @@
-ECHO is on.
+// ECHO is on.
+
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+
+const app = express();
+
+
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.use(express.static("public"));
+
+app.set("view engine", "ejs");
+
+app.get("/", function(req, res){
+    res.render("list")
+})
+
+
+app.listen(process.env.PORT || 3000, function(){
+    console.log("Server is running on port 3000.");
+});
+
