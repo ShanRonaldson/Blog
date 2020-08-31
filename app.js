375d33f1b8721c748d0fe8e92cc65b1750da4c09
Shannon Schrauwen <shannon.schrauwen@gmail.com>
2020-08-31 10:44:28 +0300

    more starting files to match App brewery

--- app.js
+++ app.js
@@ -4,16 +4,21 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
-const https = require("https");
+// const https = require("https");
+
+const homeStartingContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua";
+const aboutContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+const contactContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
 
 const app = express();
 
+app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({extended:true}));
 
 app.use(express.static("public"));
 
-app.set("view engine", "ejs");
+
 
 app.get("/", function(req, res){
     res.render("list");
