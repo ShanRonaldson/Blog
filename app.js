d985f662793f052e96d119175c02c119847baca1
Shannon Schrauwen <shannon.schrauwen@gmail.com>
2020-09-01 17:24:58 +0300

    post content to home page

--- app.js
+++ app.js
@@ -1,53 +1,64 @@
 // ECHO is on.
 
-
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
 // const https = require("https");
 
-const homeStartingContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua";
-const aboutContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
-const contactContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+const homeStartingContent =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua";
+const aboutContent =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+const contactContent =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
 
 const app = express();
 
+const posts = [];
+
 app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
 
-
-app.get("/", function(req, res){
-    res.render("home", {homeContent:homeStartingContent}); // render the above homeStartingContent on the home page
+app.get("/", function (req, res) {
+  res.render("home", { 
+      homeContent: homeStartingContent, 
+      posts: posts });
+    
 });
 
-app.get("/about", function(req, res){
-    res.render("about", {aboutContent : aboutContent});
+app.get("/about", function (req, res) {
+  res.render("about", { aboutContent: aboutContent });
 });
 
-app.get("/compose", function(req, res){
-    res.render("compose");
+app.get("/compose", function (req, res) {
+  res.render("compose");
 });
 
-app.post("/compose", function(req, res){
-    console.log(req.body.postTitle);
+app.post("/compose", function (req, res) {
+  const post = {
+    title: req.body.postTitle,
+    content: req.body.postBody,
+  };
+
+  posts.push(post);
+
+  res.redirect("/");
 });
 
-app.get("/contact", function(req, res){
-    res.render("contact", {contactContent : contactContent});
+app.get("/contact", function (req, res) {
+  res.render("contact", { contactContent: contactContent });
 });
 
-app.get("/post", function(req, res){
-    res.render("post");
+app.get("/post", function (req, res) {
+  res.render("post");
 });
 
-app.listen(process.env.PORT || 3000, function(){
-    console.log("Server is running on port 3000.");
+app.listen(process.env.PORT || 3000, function () {
+  console.log("Server is running on port 3000.");
 });
-
-
 
 // var header = document.getElementById("navBar");
 // var nav = document.getElementsByClassName("nav");
