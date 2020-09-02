2f78577e1ef7737dd3ea45454490b5eb51807395
Shannon Schrauwen <shannon.schrauwen@gmail.com>
2020-09-02 13:40:07 +0300

    addition of lodash creating new post pages when new posts are created.

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const request = require("request");
+const _ = require("lodash");
 // const https = require("https");
 
 const homeStartingContent =
@@ -23,10 +24,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-  res.render("home", { 
-      homeContent: homeStartingContent, 
-      posts: posts });
-    
+  res.render("home", {
+    homeContent: homeStartingContent,
+    posts: posts,
+  });
 });
 
 app.get("/about", function (req, res) {
@@ -39,7 +40,7 @@ app.get("/compose", function (req, res) {
 
 app.post("/compose", function (req, res) {
   const post = {
-    title: req.body.postTitle,
+    title: req.body.postTitle, //creates a JS object for us to tap into and use in other areas
     content: req.body.postBody,
   };
 
@@ -49,12 +50,30 @@ app.post("/compose", function (req, res) {
 });
 
 app.get("/contact", function (req, res) {
-  res.render("contact", { contactContent: contactContent });
+  res.render("contact", { contactContent: contactContent }); //show content from above on the contact page
 });
 
-app.get("/post", function (req, res) {
-  res.render("post");
+app.get("/posts/:postName", function (req, res) {
+  const requestedTitle = _.lowerCase(req.params.postName); // make sure it doesnt matter what the user types, the browser will pick it up
+
+  posts.forEach(function (post) {
+    const storedTitle = _.lowerCase(post.title);
+
+    if (storedTitle === requestedTitle) {
+      res.render("post", {
+        title: post.title,
+        content: post.content,
+      });
+    }
+  });
+
 });
+
+// app.get("/post", function (req, res) {
+//   res.render("post");
+// });
+
+
 
 app.listen(process.env.PORT || 3000, function () {
   console.log("Server is running on port 3000.");
