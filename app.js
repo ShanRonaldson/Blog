dd46995b65cf2d2cc003e30e55376b74e34d9321
Shannon Schrauwen <shannon.schrauwen@gmail.com>
2020-09-10 13:46:54 +0300

    save posts to database

--- app.js
+++ app.js
@@ -2,9 +2,30 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
+const ejs = require("ejs");
 const request = require("request");
 const _ = require("lodash");
-// const https = require("https");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/blogDB", {
+  useUnifiedTopology: true,
+  useNewUrlParser: true,
+});
+
+const blogSchema = {
+  title: String,
+  content: String,
+};
+
+const Post = mongoose.model("Post", blogSchema);
 
 const homeStartingContent =
   "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua";
@@ -13,25 +34,13 @@ const aboutContent =
 const contactContent =
   "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
 
-const app = express();
-
-const posts = [];
-
-app.set("view engine", "ejs");
-
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use(express.static("public"));
-
 app.get("/", function (req, res) {
-  res.render("home", {
-    homeContent: homeStartingContent,
-    posts: posts,
+  Post.find({}, function (err, posts) {
+    res.render("home", {
+      homeContent: homeStartingContent,
+      posts: posts,
+    });
   });
-});
-
-app.get("/about", function (req, res) {
-  res.render("about", { aboutContent: aboutContent });
 });
 
 app.get("/compose", function (req, res) {
@@ -39,54 +48,37 @@ app.get("/compose", function (req, res) {
 });
 
 app.post("/compose", function (req, res) {
-  const post = {
-    title: req.body.postTitle, //creates a JS object for us to tap into and use in other areas
+  const post = new Post({
+    title: req.body.postTitle,
     content: req.body.postBody,
-  };
+  });
 
-  posts.push(post);
+  post.save(function (err) {
+    if (!err) {
+      res.redirect("/");
+    }
+  });
+});
 
-  res.redirect("/");
+app.get("/posts/:postId", function (req, res) {
+  const requestedPostId = req.params.postId;
+
+  Post.findOne({ _id: requestedPostId }, function (err, post) {
+    res.render("post", {
+      title: post.title,
+      content: post.content,
+    });
+  });
+});
+
+app.get("/about", function (req, res) {
+  res.render("about", { aboutContent: aboutContent });
 });
 
 app.get("/contact", function (req, res) {
   res.render("contact", { contactContent: contactContent }); //show content from above on the contact page
 });
 
-app.get("/posts/:postName", function (req, res) {
-  const requestedTitle = _.lowerCase(req.params.postName); // make sure it doesnt matter what the user types, the browser will pick it up
-
-  posts.forEach(function (post) {
-    const storedTitle = _.lowerCase(post.title);
-
-    if (storedTitle === requestedTitle) {
-      res.render("post", {
-        title: post.title,
-        content: post.content,
-      });
-    }
-  });
-
-});
-
-// app.get("/post", function (req, res) {
-//   res.render("post");
-// });
-
-
-
 app.listen(process.env.PORT || 3000, function () {
   console.log("Server is running on port 3000.");
 });
-
-// var header = document.getElementById("navBar");
-// var nav = document.getElementsByClassName("nav");
-
-// for (var i = 0; i < nav.length; i ++){
-
-//     nav[i].addEventListener("click", function(){
-//         var current = document.getElementsByClassName("active");
-//         current[0].className = current[0].className.replace(" active", "");
-//         this.className ++ " active";
-//     });
-// }
